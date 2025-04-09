8de120cbe55ec1b150f5f94e6e85f1a2e7c4a50d
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-09 12:26:25 +0600

    fetch data using useEffect from fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Navbar from './components/navbar'
+import Banner from './components/banner'
+import Blogs from './components/Blogs'
 
 function App() {
 
   return (
     <>
       <Navbar></Navbar>
+      <Banner></Banner>
+      <Blogs></Blogs>
     </>
   )
 }
---
+++ src/components/blogs.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+const Blogs = () => {
+    const [blogs,setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    }, [])
+
+    return (
+        <div>
+            {
+                blogs.map(blog => console.log(blog))
+            }
+        </div>
+    );
+};
+
+export default Blogs;
